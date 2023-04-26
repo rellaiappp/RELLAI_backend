b081d181b34ab0029f444ab1991b4f2acef5c0a0
rellaiappp <131451509+rellaiappp@users.noreply.github.com>
2023-04-26 11:54:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ async def root():
 app.include_router(api_router,prefix="/api/v1")
 
 if __name__ == "__main__":
-   uvicorn.run("main:app", host="0.0.0.0", port=8000)
+   uvicorn.run("main:app", host="0.0.0.0", port=8080)
