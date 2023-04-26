7779555228c3048059925654739cb1e7ac80fc6b
rellaiappp <131451509+rellaiappp@users.noreply.github.com>
2023-04-26 11:43:17 +0200

    Update for cloud

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
 from api.api_v1.api import router as api_router
 import uvicorn
-import firebase_admin
-import pyrebase
-from firebase_admin import credentials, auth
 from fastapi.middleware.cors import CORSMiddleware
 import json
 
-cred = credentials.Certificate('eeee_service_account_keys.json')
-firebase = firebase_admin.initialize_app(cred)
-pb = pyrebase.initialize_app(json.load(open('../rellai_backend_firebase.json')))
 app = FastAPI()
 
 allow_all = ['*']
