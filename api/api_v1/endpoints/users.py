ece72984c6f91d07d7a76895d359f594df7d077f
rellaiappp <131451509+rellaiappp@users.noreply.github.com>
2023-04-26 12:07:43 +0200

    updated requirements.txt

--- api/api_v1/endpoints/users.py
+++ api/api_v1/endpoints/users.py
@@ -1,7 +1,8 @@
+# import firebase_admin
 from fastapi import APIRouter, Request
-from fastapi.responses import JSONResponse
-from fastapi.exceptions import HTTPException
-from firebase_admin import credentials, auth
+# from fastapi.responses import JSONResponse
+# from fastapi.exceptions import HTTPException
+# from firebase_admin import credentials, auth
 
 router = APIRouter()
 
@@ -9,15 +10,16 @@ router = APIRouter()
 async def get_users():
     return {"message":"Users!"}
 
-def check_token(token):
-    try:
-        decoded_token = auth.verify_id_token(token)
-        return decoded_token['uid']
-    except:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+# def check_token(token):
+#     try:
+#         decoded_token = auth.verify_id_token(token)
+#         return decoded_token['uid']
+#     except:
+#         raise HTTPException(status_code=401, detail="Unauthorized")
     
-@router.post("/")
-async def create_user(request: Request):
-    token = request.headers['Authorization']
-    uid = check_token(token)
-    return uid
+# @router.post("/")
+# # async def create_user(request: Request):
+# #     token = request.headers['Authorization']
+# #     #uid = check_token(token)
+# #     return uid
+
