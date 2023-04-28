19f87a8b22f69fd5e7834f8c7bc5f1ee3b15c99e
rellaiappp <131451509+rellaiappp@users.noreply.github.com>
2023-04-28 11:21:18 +0200

    Functioning

--- api/api_v1/endpoints/functions.py
+++ api/api_v1/endpoints/functions.py
@@ -1,15 +1,23 @@
 import smtplib
 import traceback
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def load_email_code(file):
+    HTMLFile = open(file, "r")
+    data = HTMLFile.read()
+    return data
 
 def sendConfirmationEmail(receiver, project_id):
+    body = load_email_code('api/api_v1/endpoints/mail_template.html')
     sender = 'info@rellai.com'
     receivers = [receiver]
-    msg = MIMEText('This is test mail')
+    msg = MIMEMultipart()
 
     msg['Subject'] = f'Project id confirmation: {project_id}'
     msg['From'] = 'info@rellai.com'
     msg['To'] = receiver
+    msg.attach(MIMEText(body, 'html'))
 
     try:
         with smtplib.SMTP('smtp.hostinger.com', 587) as server:
@@ -20,3 +28,4 @@ def sendConfirmationEmail(receiver, project_id):
     except Exception as e:
         print(e)
         return False
+
